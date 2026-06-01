06c0e330a579cad66341ea0133e1c7a88a1b6643
ParcheVerde <guevararamirez121@gmail.com>
2026-05-31 22:30:48 -0600

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Ferreteri.Models;
+using Ferreteri.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddScoped<ICategoriasService, CategoriasService>();
+builder.Services.AddScoped<IProductosService, ProductosService>();
+builder.Services.AddScoped<IMovimientosService, MovimientosService>();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//cadena de conexion a nuestra base de datos
+builder.Services.AddDbContext<FerreteriContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
